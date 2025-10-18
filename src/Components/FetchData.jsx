0b45348002debdaee5ec1005f739dba0ac20378f
SellFares <SWE2409146@xmu.edu.my>
2025-10-19 00:35:42 +0800

    Update FetchData.jsx

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import UseFetch from './UseFetch';
+import './FetchData.css'
 
 const FetchData = () => {
 
@@ -12,6 +13,12 @@ const FetchData = () => {
         <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
         { data && data.map ( (e) => (
             <>
+                <li key={index} className='list_data'>
+                    <h3>{e.name}</h3>
+                    <p><strong>Importance: </strong>{e.importance}</p>
+                    <p><strong>Benefits: </strong>{e.benefits}</p>
+                    <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p>
+                </li>
             </>
         ) ) }
      </ul>
